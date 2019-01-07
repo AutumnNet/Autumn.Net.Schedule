d6492a0aea791eb1c7de5244efc149fb1ddb9754
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2019-01-07 15:06:48 +0200

    init

---
+++ Autumn.Schedule/ScheduledAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Autumn.Schedule
+{
+    /// <summary>
+    /// Scheduler
+    /// </summary>
+    public class ScheduledAttribute: Attribute
+    {
+        /// <summary>
+        /// Which specifies the interval between method
+        /// invocations measured from the start time of
+        /// each invocation
+        /// </summary>
+        public int FixedRate { get; set; }
+        
+        /// <summary>
+        /// Which specifies the interval between invocations
+        /// measured from the completion of the task
+        /// </summary>
+        public int FixedDelay { get; set; }
+        
+        /// <summary>
+        /// Value name
+        /// Which specifies the interval between method
+        /// invocations measured from the start time of
+        /// each invocation 
+        /// </summary>
+        public string FixedDelayString { get; set; }
+        
+        /// <summary>
+        /// Value name
+        /// Which specifies the interval between invocations
+        /// measured from the completion of the task
+        /// </summary>
+        public string FixedRateString { get; set; }
+        
+        /// <summary>
+        /// Crontab Format
+        /// </summary>
+        public string Cron { get; set; }
+    }
+}
---
+++ Autumn.Schedule/SchedulledComponentProcessor.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Autumn.Net.Annotation;
+using Autumn.Net.Engine;
+using Autumn.Net.Interfaces;
+
+namespace Autumn.Schedule
+{
+    [Service]
+    public class SchedulledComponentProcessor : IAutumnComponentInitializationProcessor
+    {
+        class MethodContainer : IDisposable
+        {
+            public object Target { get; set; }
+            public MethodInfo Method { get; set; }
+            public ScheduledAttribute Scheduled { get; set; }
+            
+            public Thread thread;
+            
+
+            private void Execute()
+            {
+                while (true)
+                {
+                    try {
+                        Thread.Sleep(Scheduled.FixedRate);
+                        Method.Invoke(Target, new object[0]);
+                    } catch(Exception) { }
+                }
+            }
+            
+            public MethodContainer Start(ApplicationContext ctx)
+            {
+                thread = new Thread(Execute);
+                thread.Start();
+                return this;
+            }
+
+            public void Dispose()
+            {
+                thread.Abort();
+            }
+        }
+
+        private ApplicationContext ctx;
+
+        private List<MethodContainer> Methods { get; set; }
+
+        [PreDestroy]
+        public void PreDestroy()
+        {
+            Methods.ForEach(item => item.Dispose());
+            Methods = null;
+        }
+        
+        static IEnumerable<MethodInfo> IsAutumnScheduledAttribute(Type type)
+        {
+            return type
+                .GetMethods()
+                .Where(item => item.GetCustomAttributes(typeof(ScheduledAttribute), true).Length > 0);
+        }
+        
+        public void Process(object o, ApplicationContext ctx)
+        {
+            this.ctx = ctx;
+            Methods = new List<MethodContainer>();
+            foreach (var methodInfo in IsAutumnScheduledAttribute(o.GetType())) {
+               Methods.Add(new MethodContainer{
+                   Target = o,
+                   Method = methodInfo,
+                   Scheduled = methodInfo.GetCustomAttribute<ScheduledAttribute>()
+               }.Start(ctx)); 
+            }
+        }     
+    }
+}
